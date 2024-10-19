b7d98e7e98bc771e74d5a5edb689a091df9b5ef6
hovhannes <johnnysc091@gmail.com>
2024-10-19 13:58:32 +0900

    step 6: code for step 5

--- app/src/main/java/ru/easycode/myunittestingcodegenksptddlib/MyLib.kt
+++ app/src/main/java/ru/easycode/myunittestingcodegenksptddlib/MyLib.kt
@@ -1,9 +1,22 @@
 package ru.easycode.myunittestingcodegenksptddlib
 
+import android.os.Build
+import androidx.annotation.RequiresApi
+
 class MyLib {
 
-    fun generate(clasz: Class<*>) {
+    @RequiresApi(Build.VERSION_CODES.S)
+    fun generate(clasz: Class<*>) : String {
         if (!clasz.isInterface)
             throw IllegalStateException("${clasz.simpleName}::class.java is not an interface!")
+
+        val stringBuilder = StringBuilder()
+
+        stringBuilder.append(clasz.`package`)
+        stringBuilder.append("\n\n")
+        stringBuilder.append("import junit.framework.TestCase.assertEquals")
+        stringBuilder.append("\n\n")
+
+        return stringBuilder.toString()
     }
 }
