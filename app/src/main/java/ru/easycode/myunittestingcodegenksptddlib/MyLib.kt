30cd99d942c385950b9287638aa63e22be040acd
hovhannes <johnnysc091@gmail.com>
2024-10-27 11:49:06 +0900

    step 16 code for step 15

--- app/src/main/java/ru/easycode/myunittestingcodegenksptddlib/MyLib.kt
+++ app/src/main/java/ru/easycode/myunittestingcodegenksptddlib/MyLib.kt
@@ -2,7 +2,7 @@ package ru.easycode.myunittestingcodegenksptddlib
 
 class MyLib {
 
-    fun generate(clasz: Class<*>) : String {
+    fun generate(clasz: Class<*>): String {
         val interfaceName = clasz.simpleName
         if (!clasz.isInterface) {
             throw IllegalStateException("$interfaceName::class.java is not an interface!")
@@ -18,28 +18,32 @@ class MyLib {
         stringBuilder.append(interfaceName)
         stringBuilder.append(" : ")
         stringBuilder.append(interfaceName)
-        stringBuilder.append(" {\n\n")
+        stringBuilder.append(" {\n")
 
-        stringBuilder.append("    private var ")
-        val methodName = clasz.declaredMethods.first().name
-        stringBuilder.append(methodName)
-        stringBuilder.append("CalledTimes: Int = 0\n\n")
-        stringBuilder.append("    fun assert")
-        val methodNamePascalCase = methodName.replaceFirstChar { it.uppercase() }
-        stringBuilder.append(methodNamePascalCase)
-        stringBuilder.append("CalledTimes(expected: Int) {\n")
-        stringBuilder.append("        assertEquals(expected, ")
-        stringBuilder.append(methodName)
-        stringBuilder.append("CalledTimes)\n")
-        stringBuilder.append("    }\n\n")
+        clasz.declaredMethods.forEach { method ->
+            stringBuilder.append("\n")
+            stringBuilder.append("    private var ")
+            val methodName = method.name
+            stringBuilder.append(methodName)
+            stringBuilder.append("CalledTimes: Int = 0\n\n")
+            stringBuilder.append("    fun assert")
+            val methodNamePascalCase = methodName.replaceFirstChar { it.uppercase() }
+            stringBuilder.append(methodNamePascalCase)
+            stringBuilder.append("CalledTimes(expected: Int) {\n")
+            stringBuilder.append("        assertEquals(expected, ")
+            stringBuilder.append(methodName)
+            stringBuilder.append("CalledTimes)\n")
+            stringBuilder.append("    }\n\n")
 
-        stringBuilder.append("    override fun ")
-        stringBuilder.append(methodName)
-        stringBuilder.append("() {\n")
-        stringBuilder.append("        ")
-        stringBuilder.append(methodName)
-        stringBuilder.append("CalledTimes++\n")
-        stringBuilder.append("    }\n")
+            stringBuilder.append("    override fun ")
+            stringBuilder.append(methodName)
+            stringBuilder.append("() {\n")
+            stringBuilder.append("        ")
+            stringBuilder.append(methodName)
+            stringBuilder.append("CalledTimes++\n")
+            stringBuilder.append("    }\n")
+        }
+
         stringBuilder.append("}")
 
         return stringBuilder.toString()
